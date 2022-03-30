5f569cad45a6cffb39c4c296121e4dc6edb6d08a
hasanvajdi <hasanvajdi9@gmail.com>
2022-03-30 13:14:12 +0430

    style | add spinner and success result with antdesign for login - remaine timeOut for success result

--- nextjs/todo/pages/account/login.js
+++ nextjs/todo/pages/account/login.js
@@ -1,9 +1,12 @@
+//React
+import { useState } from 'react';
+//other
 import accountStyle from '../../styles/account.module.scss'
 import axios from 'axios';
 import Header from '../../components/Header'
 //adnt design
-import { Row, Col, Form, Input, Button, Checkbox  } from 'antd';
-import { UserOutlined, LockOutlined, GithubOutlined, GoogleOutlined, LinkedinOutlined } from '@ant-design/icons';
+import { Row, Col, Form, Input, Button, Checkbox, Spin, Result } from 'antd';
+import { LoadingOutlined, UserOutlined, LockOutlined, GithubOutlined, GoogleOutlined, LinkedinOutlined } from '@ant-design/icons';
 //react query
 import { useMutation } from "react-query"
 //next js
@@ -20,6 +23,8 @@ const loginExpressApi = async (values)=>{
 
 const login = ()=>{
     const nextRouter = useRouter();
+    const [spinState, setSpinState] = useState(false)
+    const [indicatorIcon, setIndicatorIcon] = useState(<LoadingOutlined style={{ fontSize: 50, color:"black" }}/>)
 
     const { mutate:loginMutate } = useMutation(loginExpressApi, {
         onError:(err)=>{
@@ -38,141 +43,152 @@ const login = ()=>{
             }
         },
         onSuccess:()=>{
+            //const myTimeout = setTimeout(()=>{null}, 10000);
+            //clearTimeout(myTimeout);
+            setIndicatorIcon(<Result
+                status="success"
+                title="اطلاعات شما صحیح بود"
+                subTitle="در حال پردازش برای انتقال به پروفایل شما"
+            />)
             nextRouter.push("profile")
             console.log("login success")
         }
     })
     
     const onFinish = (values) => {
+        setSpinState(true)
         loginMutate(values)
+
     };
 
     return(
-       <div className={accountStyle.allElements}>
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-            <Row className={accountStyle.headerRow}>
-                <Col className={accountStyle.headerCol} span={24}>
-                    <img src="/account-header.svg" className={accountStyle.headerImage }/>
-                </Col>
-                <Col className={accountStyle.loginHeaderCol}>
-                    <Header />
-                </Col>
-            </Row>
+        <Spin spinning={spinState} indicator={indicatorIcon}>
+            <div className={accountStyle.allElements}>
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
+                <Row className={accountStyle.headerRow}>
+                    <Col className={accountStyle.headerCol} span={24}>
+                        <img src="/account-header.svg" className={accountStyle.headerImage }/>
+                    </Col>
+                    <Col className={accountStyle.loginHeaderCol}>
+                        <Header />
+                    </Col>
+                </Row>
 
-            <Row className={`${accountStyle.mainRow} animate__animated animate__fadeInRight animate__delay-0.5s`} align="top" justify='start' wrap="false">
-                <Col className={accountStyle.allOfForm} span={24}>
-                    <Row>
-                        <Col span={24} className={accountStyle.loginFormHeader}>
-                            <UserOutlined className={accountStyle.loginFormUserIcon}/>
-                            <span className={accountStyle.loginFormLoginText}>ورود</span>
-                        </Col>
-                    </Row>
+                <Row className={`${accountStyle.mainRow} animate__animated animate__fadeInRight animate__delay-0.5s`} align="top" justify='start' wrap="false">
+                    <Col className={accountStyle.allOfForm} span={24}>
+                        <Row>
+                            <Col span={24} className={accountStyle.loginFormHeader}>
+                                <UserOutlined className={accountStyle.loginFormUserIcon}/>
+                                <span className={accountStyle.loginFormLoginText}>ورود</span>
+                            </Col>
+                        </Row>
 
-                    <Row className={accountStyle.selfFormRow}>
-                        <Col span={24} className={accountStyle.selfFormCol}>
-                          
-                           <Form
-                                name="normal_login"
-                                className={accountStyle.selfLoginForm}
-                                size="large"
-                                initialValues={{
-                                    remember: true,
-                                }}
-                                onFinish={onFinish}
-                            >
-
-                                <Form.Item
-                                    name="username"
-                                    rules={[
-                                    {
-                                        required: true,
-                                        message: 'لطفا نام کاربری را وارد کنید!',
-                                    },
-                                    ]}
+                        <Row className={accountStyle.selfFormRow}>
+                            <Col span={24} className={accountStyle.selfFormCol}>
+                            
+                            <Form
+                                    name="normal_login"
+                                    className={accountStyle.selfLoginForm}
+                                    size="large"
+                                    initialValues={{
+                                        remember: true,
+                                    }}
+                                    onFinish={onFinish}
                                 >
-                                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="نام کاربری" />
-                                </Form.Item>
 
-                                <Form.Item
-                                    name="password"
-                                    style={{marginTop:"10px"}}
-                                    rules={[
-                                    {
-                                        required: true,
-                                        message: 'لطفا رمز عبور را وارد کنید!',
-                                    },
-                                    ]}
-                                >
-                                    <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="رمز عبور" />
-                                </Form.Item>
-
-                                
-
-                                <Form.Item>
-                                    <Form.Item  valuePropName="checked" noStyle>
-                                        <Checkbox>من را بخاطر بسپار</Checkbox>
+                                    <Form.Item
+                                        name="username"
+                                        rules={[
+                                        {
+                                            required: true,
+                                            message: 'لطفا نام کاربری را وارد کنید!',
+                                        },
+                                        ]}
+                                    >
+                                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="نام کاربری" />
                                     </Form.Item>
-                                    <a className={accountStyle.loginForgot} href="">
-                                        رمز عبورم را فرموش کردم
-                                    </a>
-                                </Form.Item>
 
-                                <Form.Item>
-                                    <Button type="primary" htmlType="submit" className={accountStyle.loginFormButton}>
-                                        ورود
-                                    </Button>
+                                    <Form.Item
+                                        name="password"
+                                        style={{marginTop:"10px"}}
+                                        rules={[
+                                        {
+                                            required: true,
+                                            message: 'لطفا رمز عبور را وارد کنید!',
+                                        },
+                                        ]}
+                                    >
+                                        <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="رمز عبور" />
+                                    </Form.Item>
 
-                                    <div className={accountStyle.loginCreateAccount}><span><Link href="/account/signup"><a>ساخت حساب جدید</a></Link></span></div>
-                                </Form.Item>
+                                    
 
-                            </Form>
+                                    <Form.Item>
+                                        <Form.Item  valuePropName="checked" noStyle>
+                                            <Checkbox>من را بخاطر بسپار</Checkbox>
+                                        </Form.Item>
+                                        <a className={accountStyle.loginForgot} href="">
+                                            رمز عبورم را فرموش کردم
+                                        </a>
+                                    </Form.Item>
+
+                                    <Form.Item>
+                                        <Button type="primary" htmlType="submit" className={accountStyle.loginFormButton}>
+                                            ورود
+                                        </Button>
+
+                                        <div className={accountStyle.loginCreateAccount}><span><Link href="/account/signup"><a>ساخت حساب جدید</a></Link></span></div>
+                                    </Form.Item>
+
+                                </Form>
 
 
-                        </Col>
-                    </Row>
+                            </Col>
+                        </Row>
 
-                    <Row className={accountStyle.loginSocial}>
+                        <Row className={accountStyle.loginSocial}>
 
-                        <Col span={7} className={accountStyle.loginGithub}>
-                            <GithubOutlined className={accountStyle.socialLoginIcon}/>
-                            <span>Github</span>
-                        </Col>
+                            <Col span={7} className={accountStyle.loginGithub}>
+                                <GithubOutlined className={accountStyle.socialLoginIcon}/>
+                                <span>Github</span>
+                            </Col>
 
-                        <Col span={7} className={accountStyle.loginGmail}>
-                            <GoogleOutlined className={accountStyle.socialLoginIcon}/>
-                            <span>Gmail</span>
-                        </Col>
+                            <Col span={7} className={accountStyle.loginGmail}>
+                                <GoogleOutlined className={accountStyle.socialLoginIcon}/>
+                                <span>Gmail</span>
+                            </Col>
 
-                        <Col span={7} className={accountStyle.loginLinkedin}>
-                            <LinkedinOutlined className={accountStyle.socialLoginIcon}/>
-                            <span>Linkedin</span>
+                            <Col span={7} className={accountStyle.loginLinkedin}>
+                                <LinkedinOutlined className={accountStyle.socialLoginIcon}/>
+                                <span>Linkedin</span>
 
-                        </Col>
+                            </Col>
 
-                    </Row>
-                </Col>
+                        </Row>
+                    </Col>
+                    
+                    
+                </Row>
                 
-                
-            </Row>
-            
-            <Row className={`${accountStyle.loginSideTextRow} animate__animated animate__fadeIn animate__delay-1s`}>
-                <Col span={20} className={accountStyle.loginSideTextCol} >
-                    <p>
-                    لورم ایپسوم یا طرح‌نما طراحی گرافیک گفته می‌شود. طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیرآنها با استفاده از خود را صفحه‌آرای را به پایان برند.                    </p>
-                </Col>
-            </Row>
+                <Row className={`${accountStyle.loginSideTextRow} animate__animated animate__fadeIn animate__delay-1s`}>
+                    <Col span={20} className={accountStyle.loginSideTextCol} >
+                        <p>
+                        لورم ایپسوم یا طرح‌نما طراحی گرافیک گفته می‌شود. طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیرآنها با استفاده از خود را صفحه‌آرای را به پایان برند.                    </p>
+                    </Col>
+                </Row>
 
-       </div>
+        </div>
+       </Spin>
 
         
     )
