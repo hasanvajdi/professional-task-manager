51dad19b76b5c6d005da419a440523a7b7179c03
hasanvajdi <hasanvajdi9@gmail.com>
2022-03-30 12:09:36 +0430

    feat | add new error handling in signup

--- nextjs/todo/pages/api/account/signup.js
+++ nextjs/todo/pages/api/account/signup.js
@@ -32,7 +32,13 @@ const nextSignup = async (req, res)=>{
         }
         else if (err.response.data.username){
             errorText = "این نام کاربری قبلا ثبت شده است"
-        }        
+        }
+        else if (err.response.data.password1){
+            errorText = "رمز وارد شده کوتاه است. حداقل 8 کاراکتر وارد کنید"
+        }
+        else if (err.response.data.password2){
+            errorText = "تکرار رمز عبور نمی تواند خالی باشد"
+        }      
         res.status(400).json({"error":`${errorText}`})
     }
 
