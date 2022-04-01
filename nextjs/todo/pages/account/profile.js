99202d84c21f7509dc91261ccdc516815f8c7757
hasanvajdi <hasanvajdi9@gmail.com>
2022-04-01 10:24:52 +0430

    style | work on profile page

--- nextjs/todo/pages/account/profile.js
+++ nextjs/todo/pages/account/profile.js
@@ -1,6 +1,39 @@
+import { Row, Col } from 'antd';
+import { CheckCircleOutlined, LoadingOutlined, FolderOpenOutlined } from '@ant-design/icons';
+import profileStyle from '../../styles/profile.module.scss'
+
+
 const profile = ()=>{
     return(
-        <span>profileleeeeeeee</span>
+        <>
+            <div className={profileStyle.profileContainer}>
+                <Row className={profileStyle.profileHeader}>
+                </Row>
+
+                <Row className={profileStyle.profileBody}  align="middle" justify="center">
+                    <Col className={profileStyle.allTasks} span={7} >
+                        <div className={profileStyle.allTasksHeader}>
+                            <span>همه</span>
+                            <FolderOpenOutlined className={profileStyle.allTasksIcon} />
+                        </div>
+                    </Col>
+
+                    <Col className={profileStyle.doingTasks} span={7} >
+                        <div className={profileStyle.doingTasksHeader}>
+                            <span>در حال انجام</span>
+                            <LoadingOutlined className={profileStyle.doingTasksIcon} />
+                        </div>
+                    </Col>
+
+                    <Col className={profileStyle.finishedTasks} span={7} >
+                        <div className={profileStyle.finishedHeader}>
+                            <span>تمام شده</span>
+                            <CheckCircleOutlined className={profileStyle.finishedTasksIcon} />
+                        </div>                    
+                    </Col>
+                </Row>
+            </div>
+        </>
     )
 }
 
