db306a126a4d9f10fc989e73f6518ba508636c0b
hasanvajdi <hasanvajdi9@gmail.com>
2022-04-01 14:08:44 +0430

    style | and dropdonw menu for user

--- nextjs/todo/pages/account/profile.js
+++ nextjs/todo/pages/account/profile.js
@@ -1,13 +1,40 @@
-import { Row, Col } from 'antd';
-import { CheckCircleOutlined, LoadingOutlined, FolderOpenOutlined } from '@ant-design/icons';
+import { Row, Col, Menu, Dropdown, } from 'antd';
+import { LogoutOutlined, UserSwitchOutlined, EditOutlined, CaretDownOutlined, UserOutlined, CheckCircleOutlined, LoadingOutlined, FolderOpenOutlined } from '@ant-design/icons';
 import profileStyle from '../../styles/profile.module.scss'
+import Image from 'next/image'
+import classnames from 'classnames';
 
+
+const userMenu = (
+    <Menu>
+      <Menu.Item key="0" icon={<EditOutlined className={profileStyle.userMenuIcon}/>} className={profileStyle.userMenuItem}>
+        <span>ویرایش پروفایل</span>
+      </Menu.Item>
+
+      <Menu.Item key="1" icon={<UserSwitchOutlined className={profileStyle.userMenuIcon}/>} className={profileStyle.userMenuItem}>
+        <span>وظایف شخصی</span>
+      </Menu.Item>
+
+      <Menu.Divider />
+
+      <Menu.Item key="3" icon={<LogoutOutlined className={[profileStyle.userMenuIcon]} style={{color:"red"}}/>} className={[profileStyle.userMenuItem, profileStyle.userMenuLogout]}>
+        <sapn>خروج از حساب</sapn>
+      </Menu.Item>
+    </Menu>
+);
 
 const profile = ()=>{
+
     return(
         <>
             <div className={profileStyle.profileContainer}>
                 <Row className={profileStyle.profileHeader}>
+                    <Col className={profileStyle.headerUserContainer} >
+                        <UserOutlined className={profileStyle.headerUserIcon} />
+                        <Dropdown overlay={userMenu} trigger={['click']}>
+                            <CaretDownOutlined className={profileStyle.headerNavIcon} />
+                        </Dropdown>
+                    </Col>
                 </Row>
 
                 <Row className={profileStyle.profileBody}  align="middle" justify="center">
