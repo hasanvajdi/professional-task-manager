13b6faa88e6737a2ee59cf4a4beb32e1d9bcb431
hasanvajdi <hasanvajdi9@gmail.com>
2022-04-06 13:54:17 +0430

    feat | add Profile model, viewset and serializer

--- django/core/todo/models.py
+++ django/core/todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+def user_avatar(instance, filename):
+    return f'profile_avatar/{instance.user.id}_avatar.jpg'
+
+
+
+class Profile(models.Model):
+    user        = models.ForeignKey(User, on_delete=models.CASCADE) #which user realted to this profile
+    first_name  = models.CharField(max_length=50, blank=True)
+    last_name   = models.CharField(max_length=80, blank=True)
+    avatar      = models.ImageField(upload_to=user_avatar)
+    biograhpy   = models.CharField(max_length=200, blank=True)
+
--- django/core/todo/views.py
+++ django/core/todo/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from . models import Profile
 
-# Create your views here.
+class ProfileViewset(ModelViewSet):
+    queryset            = Profile.objects.all()
+    serializer_class    = ProfileSerializer
