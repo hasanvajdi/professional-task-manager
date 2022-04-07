9ff072f3553db47aab384de298e340f43b7f140a
hasanvajdi <hasanvajdi9@gmail.com>
2022-04-07 10:45:38 +0430

    feat | add Group model and tiny change in profile model

--- django/core/todo/models.py
+++ django/core/todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import random
+import string
 
 
 def user_avatar(instance, filename):
@@ -9,9 +10,26 @@ def user_avatar(instance, filename):
 
 
 class Profile(models.Model):
-    user        = models.ForeignKey(User, on_delete=models.CASCADE) #which user realted to this profile
+    user        = models.OneToOneField(User, on_delete=models.CASCADE) #which user realted to this profile
     first_name  = models.CharField(max_length=50, blank=True)
     last_name   = models.CharField(max_length=80, blank=True)
     avatar      = models.ImageField(upload_to=user_avatar)
     biograhpy   = models.CharField(max_length=200, blank=True)
 
+
+
+
+
+def group_id_creator():
+    return "-" + str(''.join(random.sample("0123456789", 10)))
+
+def group_link_creator():
+    return ''.join(random.sample("0123456789" + string.ascii_lowercase + string.ascii_uppercase, 19))
+
+
+class Group(models.Model):
+    group_id    = models.CharField(max_length=10, blank=True, unique=True, editable=False, primary_key=True, default=group_id_creator)
+    link        = models.CharField(max_length=19, blank=True, unique=True, editable=False, default=group_link_creator)
+    name        = models.CharField(max_length=50)
+    owner       = models.ForeignKey(User, on_delete=models.CASCADE)
+    members     = models.ManyToManyField(User, related_name="group_members")
